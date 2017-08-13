72b9724d646b9185533ae87db74a2bdba475271c
Robert burner Schadek <rburners@gmail.com>
2017-08-13 17:23:04 +0200

    more stronger tests

--- source/parsertests.d
+++ source/parsertests.d
@@ -15,12 +15,10 @@ unittest {
 	auto d = p.parseDocument();
 }
 
-__EOF__
-
 unittest {
 	string s = `
 {
-  query human {
+  query human() {
     name
     height
   }
@@ -36,6 +34,24 @@ unittest {
   query human(id: "1000") {
     name
     height
+  }
+}`;
+	auto l = Lexer(s);
+	auto p = Parser(l);
+	auto d = p.parseDocument();
+}
+
+unittest {
+	string s = `
+{
+  query human(id: 1000, limit: 3, gender: "male") {
+    name!
+    height
+	friends {
+		id!
+		gender
+		income
+	}
   }
 }`;
 	auto l = Lexer(s);
