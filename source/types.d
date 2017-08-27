904e1b3665329ea4bd27463ccda8ef638adc1fcd
Robert burner Schadek <robert.schadek@informatik.uni-oldenburg.de>
2017-08-27 23:18:36 +0200

    tests

--- source/types.d
+++ source/types.d
@@ -5,12 +5,20 @@ import std.typecons : Flag;
 alias NotNull = Flag!"NotNull";
 
 struct GraphQLType {
+	NotNull notNull;
 
+	this(Args...)(auto ref Args args) {
+		foreach(ref arg; args) {
+			static if(is(typeof(arg) == NotNull)) {
+				this.notNull = arg;
+			}
+		}
+	}
 }
 
 unittest {
 	static struct Foo {
-		@GraphQLType() long id;
+		@GraphQLType(NotNull.yes) long id;
 	}
 
 	Foo foo;
