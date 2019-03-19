90748653f22e157de9bb02de2998a5d2d8df1106
Robert Schadek <rschadek@symmetryinvestments.com>
2019-03-19 17:04:03 +0000

    moving stuff into main

---
+++ source/graphql.d
@@ -0,0 +1,388 @@
+module graphql;
+
+import std.array : array, front, empty;
+import std.stdio;
+import std.experimental.logger;
+import std.traits;
+import std.format : format;
+
+import vibe.data.json;
+
+import builder;
+import ast;
+import helper;
+import tokenmodule;
+import argumentextractor;
+import schema.types;
+import schema.resolver;
+
+struct DefaultContext {
+}
+
+class GraphQLD(T, QContext = DefaultContext) {
+	alias Con = QContext;
+	alias QueryResolver = Json delegate(string name, Json parent,
+			Json args, ref Con context) @safe;
+
+	alias Schema = GQLDSchema!(T);
+
+	Document doc;
+
+	Schema schema;
+
+	Con dummy;
+
+	// the logger to use
+	Logger executationTraceLog;
+	Logger defaultResolverLog;
+	Logger resolverLog;
+
+	// [Type][field]
+	QueryResolver[string][string] resolver;
+	QueryResolver defaultResolver;
+
+	this() {
+		this.schema = toSchema!(T)();
+		this.executationTraceLog = new MultiLogger(LogLevel.off);
+		this.defaultResolverLog = new MultiLogger(LogLevel.off);
+		this.resolverLog = new MultiLogger(LogLevel.off);
+
+		this.defaultResolver = delegate(string name, Json parent, Json args,
+									ref Con context)
+			{
+				import std.format;
+				this.defaultResolverLog.logf("name: %s, parent: %s, args: %s",
+						name, parent, args
+					);
+				Json ret = Json.emptyObject();
+				if(parent.type != Json.Type.null_ && name in parent) {
+					ret["data"] = Json.emptyObject();
+					ret["data"] = parent[name];
+				} else {
+					ret["error"] = Json.emptyArray();
+					ret["error"] = Json(format("no field name '%s' found",
+										name)
+									);
+				}
+				this.defaultResolverLog.logf("default ret %s", ret);
+				return ret;
+			};
+
+		setDefaultSchemaResolver(this);
+		writeln(this.schema.toString());
+	}
+
+	void setResolver(string first, string second, QueryResolver resolver) {
+		if(first !in this.resolver) {
+			this.resolver[first] = QueryResolver[string].init;
+		}
+		this.resolver[first][second] = resolver;
+	}
+
+	Json resolve(string type, string field, Json parent, Json args,
+			ref Con context)
+	{
+		Json defaultArgs = this.getDefaultArguments(type, field);
+		Json joinedArgs = joinJson(args, defaultArgs);
+		this.resolverLog.logf("%s %s %s %s %s %s", type, field,
+				defaultArgs, parent, args, joinedArgs
+			);
+		if(type !in this.resolver) {
+			return defaultResolver(field, parent, joinedArgs, context);
+		} else if(field !in this.resolver[type]) {
+			return defaultResolver(field, parent, joinedArgs, context);
+		} else {
+			return this.resolver[type][field](field, parent, joinedArgs,
+					context
+				);
+		}
+	}
+
+	Json getDefaultArgumentImpl(string typename, Type)(string type,
+			string field)
+	{
+		static if(isAggregateType!Type) {
+			if(typename == type) {
+				switch(field) {
+					static foreach(mem; __traits(allMembers, Type)) {
+						static if(isCallable!(
+								__traits(getMember, Type, mem))
+							)
+						{
+							case mem: {
+								alias parNames = ParameterIdentifierTuple!(
+										__traits(getMember, Type, mem)
+									);
+								alias parDef = ParameterDefaultValueTuple!(
+										__traits(getMember, Type, mem)
+									);
+
+								Json ret = Json.emptyObject();
+								static foreach(i; 0 .. parNames.length) {
+									static if(!is(parDef[i] == void)) {
+										ret[parNames[i]] =
+											serializeToJson(parDef[i]);
+									}
+								}
+								return ret;
+							}
+						}
+					}
+					default: break;
+				}
+			}
+		}
+		return Json.init;
+	}
+
+	Json getDefaultArguments(string type, string field) {
+		import traits : collectTypes;
+		switch(type) {
+			static foreach(Type; collectTypes!(T)) {{
+				case Type.stringof: {
+					Json tmp = getDefaultArgumentImpl!(Type.stringof, Type)(
+							type, field
+						);
+					if(tmp.type != Json.Type.undefined
+							&& tmp.type != Json.Type.null_)
+					{
+						return tmp;
+					}
+				}
+			}}
+			default: {}
+		}
+		// entryPoint == ["query", "mutation", "subscription"];
+		switch(type) {
+			static foreach(entryPoint; FieldNameTuple!T) {{
+				case entryPoint: {
+					Json tmp = getDefaultArgumentImpl!(entryPoint,
+							typeof(__traits(getMember, T, entryPoint)))
+						(type, field);
+					if(tmp.type != Json.Type.undefined
+							&& tmp.type != Json.Type.null_)
+					{
+						return tmp;
+					}
+				}
+			}}
+			default: break;
+		}
+		defaultRet:
+		return Json.init;
+	}
+
+	Json execute(Document doc, Json variables) {
+		import std.algorithm.searching : canFind, find;
+		this.doc = doc;
+		OperationDefinition[] ops = this.getOperations(this.doc);
+
+		auto selSet = ops
+			.find!(op => op.ruleSelection == OperationDefinitionEnum.SelSet);
+		if(!selSet.empty) {
+			if(ops.length > 1) {
+				throw new Exception(
+					"If SelectionSet the number of Operations must be 1"
+					);
+			}
+			return this.executeOperation(selSet.front, variables);
+		}
+
+		Json ret = returnTemplate();
+		foreach(op; ops) {
+			Json tmp = this.executeOperation(op, variables);
+			this.executationTraceLog.logf("%s\n%s", ret, tmp);
+			if(canFind([OperationDefinitionEnum.OT_N,
+					OperationDefinitionEnum.OT_N_D,
+					OperationDefinitionEnum.OT_N_V,
+					OperationDefinitionEnum.OT_N_VD], op.ruleSelection))
+			{
+				if(tmp.type == Json.Type.object && "data" in tmp) {
+					foreach(key, value; tmp["data"].byKeyValue()) {
+						if(key in ret["data"]) {
+							this.executationTraceLog.logf(
+									"key %s already present", key
+								);
+							continue;
+						}
+						ret["data"][key] = value;
+					}
+				}
+				foreach(err; tmp["error"]) {
+					ret["error"] ~= err;
+				}
+			}
+		}
+		return ret;
+	}
+
+	static OperationDefinition[] getOperations(Document doc) {
+		import std.algorithm : map;
+		return opDefRange(doc).map!(op => op.def.op).array;
+	}
+
+	Json executeOperation(OperationDefinition op, Json variables) {
+		if(op.ruleSelection == OperationDefinitionEnum.SelSet
+				|| op.ot.tok.type == TokenType.query)
+		{
+			return this.executeQuery(op, variables);
+		} else if(op.ot.tok.type == TokenType.mutation) {
+			return this.executeMutation(op, variables);
+		} else if(op.ot.tok.type == TokenType.subscription) {
+			assert(false, "Subscription not supported yet");
+		}
+		assert(false, "Unexpected");
+	}
+
+	Json executeMutation(OperationDefinition op, Json variables) {
+		log("mutation");
+		Json tmp = this.executeSelections(op.ss.sel,
+				this.schema.member["mutationType"],
+				Json.emptyObject(), variables
+			);
+		return tmp;
+	}
+
+	Json executeQuery(OperationDefinition op, Json variables) {
+		log("query");
+		Json tmp = this.executeSelections(op.ss.sel,
+				this.schema.member["queryType"],
+				Json.emptyObject(), variables
+			);
+		return tmp;
+	}
+
+	Json executeSelections(Selections sel, GQLDType objectType,
+			Json objectValue, Json variables)
+	{
+		import traits : interfacesForType;
+		Json ret = returnTemplate();
+		this.executationTraceLog.logf("OT: %s, OJ: %s, VAR: %s",
+				objectType.name, objectValue, variables);
+		this.executationTraceLog.logf("TN: %s", interfacesForType!(T)(
+				objectValue
+					.getWithDefault!string("data.__typename", "__typename")
+			));
+		foreach(FieldRangeItem field;
+				fieldRangeArr(sel, this.doc, interfacesForType!(T)(objectValue
+					.getWithDefault!string("data.__typename", "__typename")))
+			)
+		{
+			Json rslt = this.executeFieldSelection(field, objectType,
+					objectValue, variables
+				);
+			ret.insertPayload(field.name, rslt);
+		}
+		return ret;
+	}
+
+	Json executeFieldSelection(FieldRangeItem field, GQLDType objectType,
+			Json objectValue, Json variables)
+	{
+		this.executationTraceLog.logf("FRI: %s, OT: %s, OV: %s, VAR: %s",
+				field.name, objectType.name, objectValue, variables
+			);
+		Json arguments = getArguments(field, variables);
+		Json de = this.resolve(objectType.name, field.name,
+				"data" in objectValue ? objectValue["data"] : objectValue,
+				arguments, this.dummy
+			);
+		auto retType = this.schema.getReturnType(objectType, field.name);
+		if(retType is null) {
+			this.executationTraceLog.logf("ERR %s %s", objectType.name,
+					field.name
+				);
+			Json ret = Json.emptyObject();
+			ret["error"] = Json.emptyArray();
+			ret["error"] ~= Json(format(
+					"No return type for member '%s' of type '%s' found",
+					field.name, objectType.name
+				));
+			return ret;
+		}
+		this.executationTraceLog.logf("retType %s, de: %s", retType.name, de);
+		return this.executeSelectionSet(field.f.ss, retType, de, arguments);
+	}
+
+	Json executeSelectionSet(SelectionSet ss, GQLDType objectType,
+			Json objectValue, Json variables)
+	{
+		Json rslt;
+		if(GQLDMap map = objectType.toMap()) {
+			this.executationTraceLog.logf("map %s %s", map.name, ss !is null);
+			rslt = this.executeSelections(ss.sel, map, objectValue, variables);
+		} else if(GQLDNonNull nonNullType = objectType.toNonNull()) {
+			this.executationTraceLog.logf("NonNull %s",
+					nonNullType.elementType.name
+				);
+			rslt = this.executeSelectionSet(ss, nonNullType.elementType,
+					objectValue, variables
+				);
+			if(rslt.dataIsNull()) {
+				this.executationTraceLog.logf("%s", rslt);
+				rslt["error"] ~= Json("NonNull was null");
+			}
+		} else if(GQLDNullable nullType = objectType.toNullable()) {
+			this.executationTraceLog.logf("nullable %s", nullType.name);
+			rslt = this.executeSelectionSet(ss, nullType.elementType,
+					objectValue, variables
+				);
+			this.executationTraceLog.logf("IIIIIS EMPTY %s rslt %s",
+					rslt.dataIsEmpty(), rslt
+				);
+			if(rslt.dataIsEmpty()) {
+				rslt["data"] = null;
+				rslt.remove("error");
+			} else {
+			}
+		} else if(GQLDList list = objectType.toList()) {
+			this.executationTraceLog.logf("list %s", list.name);
+			rslt = this.executeList(ss, list, objectValue, variables);
+		} else if(GQLDScalar scalar = objectType.toScalar()) {
+			rslt = objectValue;
+		}
+
+		return rslt;
+	}
+
+	Json executeList(SelectionSet ss, GQLDList objectType,
+			Json objectValue, Json variables)
+	{
+		this.executationTraceLog.logf("OT: %s, OJ: %s, VAR: %s",
+				objectType.name, objectValue, variables
+			);
+		assert("data" in objectValue, objectValue.toString());
+		auto elemType = objectType.elementType;
+		this.executationTraceLog.logf("elemType %s", elemType);
+		Json ret = returnTemplate();
+		ret["data"] = Json.emptyArray();
+		foreach(Json item;
+				objectValue["data"].type == Json.Type.array
+					? objectValue["data"]
+					: Json.emptyArray()
+			)
+		{
+			this.executationTraceLog.logf("ET: %s, item %s", elemType.name,
+					item
+				);
+			Json tmp = this.executeSelectionSet(ss, elemType, item, variables);
+			if(tmp.type == Json.Type.object) {
+				if("data" in tmp) {
+					ret["data"] ~= tmp["data"];
+				}
+				foreach(err; tmp["error"]) {
+					ret["error"] ~= err;
+				}
+			} else if(!tmp.dataIsEmpty() && tmp.isScalar()) {
+				ret["data"] ~= tmp;
+			}
+		}
+		return ret;
+	}
+
+	Json getArguments(FieldRangeItem item, Json variables) {
+		auto ae = new ArgumentExtractor(variables);
+		ae.accept(cast(const(Field))item.f);
+		return ae.arguments;
+	}
+}
--- source/helper.d
+++ source/helper.d
@@ -1,5 +1,246 @@
 module helper;
 
+import std.algorithm.searching : canFind;
+import std.algorithm.iteration : splitter;
+import std.exception : enforce;
+import std.experimental.logger;
+
+import vibe.data.json;
+
+@safe:
+
+enum d = "data";
+enum e = "error";
+
+string firstCharUpperCase(string input) {
+	import std.conv : to;
+	import std.uni : isUpper, toUpper;
+	import std.array : front, popFront;
+	if(isUpper(input.front)) {
+		return input;
+	}
+
+	dchar f = input.front;
+	input.popFront();
+
+	return to!string(toUpper(f)) ~ input;
+}
+
+Json returnTemplate() {
+	Json ret = Json.emptyObject();
+	ret["data"] = Json.emptyObject();
+	ret["error"] = Json.emptyArray();
+	return ret;
+}
+
+void insertError(T)(ref Json result, T t) {
+	Json err = serializeToJson(t);
+	if(e !in result) {
+		result[e] = Json.emptyArray();
+	}
+	enforce(result[e].type == Json.Type.array);
+	if(!canFind(result[e].byValue(), err)) {
+		result[e] ~= err;
+	}
+}
+
+void insertPayload(ref Json result, string field, Json data) {
+	if(d in data) {
+		if(d !in result) {
+			result[d] = Json.emptyObject();
+		}
+		enforce(result[d].type == Json.Type.object);
+		result[d][field] = data[d];
+	}
+	if(e in data) {
+		if(e !in result) {
+			result[e] = Json.emptyArray();
+		}
+		enforce(result[e].type == Json.Type.array);
+		if(!canFind(result[e].byValue(), data[e])) {
+			result[e] ~= data[e];
+		}
+	}
+}
+
+bool isScalar(ref const(Json) data) {
+	return data.type == Json.Type.bigInt
+			|| data.type == Json.Type.bool_
+			|| data.type == Json.Type.float_
+			|| data.type == Json.Type.int_
+			|| data.type == Json.Type.string;
+}
+
+bool dataIsEmpty(ref const(Json) data) {
+	import std.experimental.logger;
+	if(data.type == Json.Type.object) {
+		foreach(key, value; data.byKeyValue()) {
+			if(key != "error" && !value.dataIsEmpty()) {
+				return false;
+			}
+		}
+		return true;
+	} else if(data.type == Json.Type.null_
+			|| data.type == Json.Type.undefined
+		)
+	{
+		return true;
+	} else if(data.type == Json.Type.array) {
+		return data.length == 0;
+	} else if(data.type == Json.Type.bigInt
+			|| data.type == Json.Type.bool_
+			|| data.type == Json.Type.float_
+			|| data.type == Json.Type.int_
+			|| data.type == Json.Type.string
+		)
+	{
+		return false;
+	}
+
+	return true;
+}
+
+unittest {
+	string t = `{
+              "kind": {},
+              "fields": null,
+              "name": {}
+            }`;
+	Json j = parseJsonString(t);
+	assert(j.dataIsEmpty());
+}
+
+bool dataIsNull(ref const(Json) data) {
+	import std.format : format;
+	enforce(data.type == Json.Type.object, format("%s", data));
+	if(const(Json)* d = "data" in data) {
+		return d.type == Json.Type.null_;
+	}
+	return false;
+}
+
+/** Merge two Json objects.
+Values in a take precedence over values in b.
+*/
+Json joinJson(Json a, Json b) {
+	// we can not merge null or undefined values
+	if(a.type == Json.Type.null_ || a.type == Json.Type.undefined) {
+		return b;
+	}
+	if(b.type == Json.Type.null_ || b.type == Json.Type.undefined) {
+		return a;
+	}
+
+	// we need objects to merge
+	if(a.type == Json.Type.object && b.type == Json.Type.object) {
+		Json ret = a.clone();
+		foreach(key, value; b.byKeyValue()) {
+			if(key !in ret) {
+				ret[key] = value;
+			}
+		}
+		return ret;
+	}
+	return a;
+}
+
+unittest {
+	Json a = parseJsonString(`{"overSize":200}`);
+	Json b = parseJsonString(`{}`);
+	Json c = joinJson(b, a);
+	assert(c == a);
+}
+
+template toType(T) {
+	import std.bigint : BigInt;
+	import std.traits : isArray, isIntegral, isAggregateType, isFloatingPoint,
+		   isSomeString;
+	static if(is(T == bool)) {
+		enum toType = Json.Type.bool_;
+	} else static if(isIntegral!(T)) {
+		enum toType = Json.Type.int_;
+	} else static if(isFloatingPoint!(T)) {
+		enum toType = Json.Type.float_;
+	} else static if(isSomeString!(T)) {
+		enum toType = Json.Type.string;
+	} else static if(isArray!(T)) {
+		enum toType = Json.Type.array;
+	} else static if(isAggregateType!(T)) {
+		enum toType = Json.Type.object;
+	} else static if(is(T == BigInt)) {
+		enum toType = Json.Type.bigint;
+	} else {
+		enum toType = Json.Type.undefined;
+	}
+}
+
+bool hasPathTo(T)(Json data, string path, ref T ret) {
+	enum TT = toType!T;
+	auto sp = path.splitter(".");
+	string f;
+	while(!sp.empty) {
+		f = sp.front;
+		sp.popFront();
+		if(data.type != Json.Type.object || f !in data) {
+			return false;
+		} else {
+			data = data[f];
+		}
+	}
+	static if(is(T == Json)) {
+		ret = data;
+		return true;
+	} else {
+		if(data.type == TT) {
+			ret = data.to!T();
+			return true;
+		}
+		return false;
+	}
+}
+
+/**
+params:
+	path = A "." seperated path
+*/
+T getWithDefault(T)(Json data, string[] paths...) {
+	enum TT = toType!T;
+	T ret = T.init;
+	foreach(string path; paths) {
+		if(hasPathTo!T(data, path, ret)) {
+			return ret;
+		}
+	}
+	return ret;
+}
+
+unittest {
+	Json d = parseJsonString(`{"error":[],"data":{"commanderId":8,
+			"__typename":"Starship","series":["DeepSpaceNine",
+			"TheOriginalSeries"],"id":43,"name":"Defiant","size":130,
+			"crewIds":[9,10,11,1,12,13,8],"designation":"NX-74205"}}`);
+	string r = d.getWithDefault!string("data.__typename");
+	assert(r == "Starship", r);
+}
+
+unittest {
+	Json d = parseJsonString(`{"commanderId":8,
+			"__typename":"Starship","series":["DeepSpaceNine",
+			"TheOriginalSeries"],"id":43,"name":"Defiant","size":130,
+			"crewIds":[9,10,11,1,12,13,8],"designation":"NX-74205"}`);
+	string r = d.getWithDefault!string("data.__typename", "__typename");
+	assert(r == "Starship", r);
+}
+
+unittest {
+	Json d = parseJsonString(`{"commanderId":8,
+			"__typename":"Starship","series":["DeepSpaceNine",
+			"TheOriginalSeries"],"id":43,"name":"Defiant","size":130,
+			"crewIds":[9,10,11,1,12,13,8],"designation":"NX-74205"}`);
+	string r = d.getWithDefault!string("__typename");
+	assert(r == "Starship", r);
+}
+
 // TODO should return ref
 auto accessNN(string[] tokens,T)(T tmp0) {
 	import std.array : back;
@@ -11,7 +252,7 @@ auto accessNN(string[] tokens,T)(T tmp0) {
 				auto tmp%d = tmp%d.%s;`(idx, idx+1, idx, token)
 			);
 		}
-		return mixin(format("tmp%d", tokens.length)); 
+		return mixin(format("tmp%d", tokens.length));
 	}
 	return null;
 }
