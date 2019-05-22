f8437998003972c17c0ccb948b5f2f36aa5b43cd
Robert Schadek <rschadek@symmetryinvestments.com>
2019-05-22 14:09:54 +0100

    argument extraction works better now

--- source/graphql/argumentextractortests.d
+++ source/graphql/argumentextractortests.d
@@ -1,8 +1,11 @@
 module graphql.argumentextractortests;
 
+import std.format : format;
+
 import vibe.data.json;
 
 import graphql.ast;
+import graphql.astselector;
 import graphql.helper : lexAndParse;
 import graphql.argumentextractor;
 import graphql.testschema;
@@ -34,7 +37,7 @@ fragment charac on Character {
   ...robot
   id
   ...hyooman
-  name
+  name(get: $s)
   series
 }`;
 
@@ -43,5 +46,35 @@ fragment charac on Character {
 	Document doc = cast()lexAndParse(q);
 	assert(doc !is null);
 
-	auto gql = new GraphQLD!Schema();
+	{
+		auto startships = astSelect!Field(doc, "a.starships");
+		assert(startships !is null);
+
+		Json args = getArguments(cast()startships, vars);
+		assert(args == parseJsonString(`{"overSize" : 10}`),
+				format("%s", args)
+			);
+	}
+
+	{
+		auto crew = astSelect!Field(doc, "a.starships.crew");
+		assert(crew !is null);
+
+		Json args = getArguments(cast()crew, vars);
+		assert(args == parseJsonString(
+					`{"if" : false}`
+				), format("%s", args)
+			);
+	}
+
+	{
+		auto name = astSelect!Field(doc, "a.starships.crew.name");
+		assert(name !is null);
+
+		Json args = getArguments(cast()name, vars);
+		assert(args == parseJsonString(
+					`{"get" : false}`
+				), format("%s", args)
+			);
+	}
 }
