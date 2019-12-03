525c7d16981614a73b47d387b0818e675994ee02
Robert Schadek <rschadek@symmetryinvestments.com>
2019-12-03 10:14:22 +0000

    less static

--- source/graphql/reflection.d
+++ source/graphql/reflection.d
@@ -4,6 +4,14 @@ import std.traits;
 import std.meta;
 import vibe.data.json;
 
+@safe:
+
+package struct TypeWithStrippedName {
+	Json typeJson;
+	string name;
+	bool canonical;
+}
+
 package struct SchemaReflection(Schema) {
 	private {
 		static SchemaReflection _instance;
@@ -18,47 +26,10 @@ package struct SchemaReflection(Schema) {
 	string[TypeInfo] classes;
 	string[][TypeInfo] derivatives;
 	string[][string] bases;
-	static struct TypeWithStrippedName {
-		Json typeJson;
-		string name;
-		bool canonical;
-	}
 
 	TypeWithStrippedName[string] jsonTypes;
 
 	private:
-
-	static void builder(T)(ref SchemaReflection ths) {
-		static if(is(T == class)) {
-			ths.classes[typeid(T)] = T.stringof;
-			foreach(B; AliasSeq!(T, TransitiveBaseTypeTuple!T)) {
-				static if(!is(B == Object)) {
-					ths.derivatives.require(typeid(B), null) ~= T.stringof;
-					ths.bases.require(T.stringof, null) ~= B.stringof;
-				}
-			}
-		} else static if(is(T == interface)) {
-			// go through all base types, and set up the derivation lines
-			foreach(B; AliasSeq!(T, InterfacesTuple!T)) {
-				ths.derivatives.require(typeid(B), null) ~= T.stringof;
-				ths.bases.require(T.stringof, null) ~= B.stringof;
-			}
-		} else {
-			// all other types have derivatives and bases of themselves
-			ths.derivatives.require(typeid(T), null) ~= T.stringof;
-			ths.bases.require(T.stringof, null) ~= T.stringof;
-		}
-	}
-
-	static void builderPhase2(T)(ref SchemaReflection ths) {
-		import graphql.schema.typeconversions : typeToTypeName, typeToJsonImpl;
-		// build the second parts which need the first parts
-		alias stripped = stripArrayAndNullable!T;
-		ths.jsonTypes[typeToTypeName!T] =
-			TypeWithStrippedName(typeToJsonImpl!(stripped, Schema, T)(),
-								 stripped.stringof,
-								 is(stripArrayAndNullable!T == T));
-	}
 
 	void initialize() @safe {
 		if(initialized) {
@@ -69,4 +40,36 @@ package struct SchemaReflection(Schema) {
 		execForAllTypes!(Schema, builder)(this);
 		execForAllTypes!(Schema, builderPhase2)(this);
 	}
+}
+
+private void builder(T,Schema)(ref SchemaReflection!Schema ths) {
+	static if(is(T == class)) {
+		ths.classes[typeid(T)] = T.stringof;
+		foreach(B; AliasSeq!(T, TransitiveBaseTypeTuple!T)) {
+			static if(!is(B == Object)) {
+				ths.derivatives.require(typeid(B), null) ~= T.stringof;
+				ths.bases.require(T.stringof, null) ~= B.stringof;
+			}
+		}
+	} else static if(is(T == interface)) {
+		// go through all base types, and set up the derivation lines
+		foreach(B; AliasSeq!(T, InterfacesTuple!T)) {
+			ths.derivatives.require(typeid(B), null) ~= T.stringof;
+			ths.bases.require(T.stringof, null) ~= B.stringof;
+		}
+	} else {
+		// all other types have derivatives and bases of themselves
+		ths.derivatives.require(typeid(T), null) ~= T.stringof;
+		ths.bases.require(T.stringof, null) ~= T.stringof;
+	}
+}
+
+private void builderPhase2(T,Schema)(ref SchemaReflection!Schema ths) {
+	import graphql.schema.typeconversions : typeToTypeName, typeToJsonImpl;
+	// build the second parts which need the first parts
+	alias stripped = stripArrayAndNullable!T;
+	ths.jsonTypes[typeToTypeName!T] =
+		TypeWithStrippedName(typeToJsonImpl!(stripped, Schema, T)(),
+							 stripped.stringof,
+							 is(stripArrayAndNullable!T == T));
 }
