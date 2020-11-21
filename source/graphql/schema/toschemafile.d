78f96ec93fdbbdbbf384c810d9f268d6de016972
Elijah Stone <estone@symmetryinvestments.com>
2020-11-20 16:40:08 -0800

    initial attempts to support generation of input types

--- source/graphql/schema/toschemafile.d
+++ source/graphql/schema/toschemafile.d
@@ -9,6 +9,11 @@ import std.format;
 import graphql.schema.types;
 import graphql.uda;
 
+private enum Visibility {
+    inputOutput,
+    outputOnly,
+}
+
 private void formIndent(Out, Args...)(ref Out o, size_t indent, string s, Args args) {
 	foreach(it; 0 .. indent) {
 		formattedWrite(o, "\t");
@@ -21,14 +26,14 @@ string schemaToString(T)() {
 	import graphql.schema.resolver;
 	auto app = appender!string();
 
-	bool[string] alreadyHandled;
+	Visibility[string] alreadyHandled;
 	auto sch = toSchema!T();
 	schemaImpl(app, sch, alreadyHandled);
 
 	return app.data;
 }
 
-private void schemaImpl(Out, T)(ref Out o, T t, ref bool[string] ah) {
+private void schemaImpl(Out, T)(ref Out o, T t, ref Visibility[string] ah) {
 	auto qms =
 			[ [ "mutationType", "mutation"]
 			, [ "queryType", "query"]
@@ -90,7 +95,7 @@ private void enumImpl(Out)(ref Out o, const(GQLDEnum) enu) {
 	formIndent(o, 0, "}");
 }
 
-private void typeImpl(Out)(ref Out o, const(GQLDType) type, ref bool[string] ah) {
+private Visibility typeImpl(Out)(ref Out o, const(GQLDType) type, ref Visibility[string] ah) {
     import std.algorithm.iteration : filter;
     import std.range.primitives : empty;
 
@@ -112,34 +117,20 @@ private void typeImpl(Out)(ref Out o, const(GQLDType) type, ref bool[string] ah)
     // code for Nullable(U).
     // An alternative would be to put this check after the member type
     // generation, but that causes problems with recursive types.
-    if (isPrimitiveType(type) || isNameSpecial(type.name) || type.toString in ah) {
-		return;
-	}
-	ah[type.toString] = true;
+    if (isPrimitiveType(type) || isNameSpecial(type.name)) {
+       return Visibility.inputOutput;
+    }
 
-    // handle typeImpl for types of members
-    if(auto map = cast(const(GQLDMap))type) {
-        foreach (val; map.member.byValue) {
-            typeImpl(o, val, ah);
-        }
-	} else if(auto nul = cast(const(GQLDNullable))type) {
-		typeImpl(o, nul.elementType, ah);
-        return;
-    } else if(auto op = cast(const(GQLDOperation))type) {
-        typeImpl(o, op.returnType, ah);
-        foreach(val; op.parameters.byValue) {
-            typeImpl(o, val, ah);
-        }
-    } else if(auto l = cast(const(GQLDList))type) {
-        typeImpl(o, l.elementType, ah);
-        return;
-    } else if(auto nn = cast(const(GQLDNonNull))type) {
-        typeImpl(o, nn.elementType, ah);
-        return;
-    } else if(auto nul = cast(const(GQLDNullable))type) {
-        typeImpl(o, nul.elementType, ah);
-        return;
+    if (auto vis = type.toString in ah) {
+		return *vis;
 	}
+
+    Visibility ret = Visibility.inputOutput;
+
+    ah[type.toString] = ret;
+	scope(exit) {
+        ah[type.toString] = ret;
+    }
 
     // if the type is an object or union with no members (or an actual scalar),
     // export it as a scalar and bail here
@@ -152,52 +143,99 @@ private void typeImpl(Out)(ref Out o, const(GQLDType) type, ref bool[string] ah)
         || cast(const(GQLDScalar))type) {
 
         formIndent(o, 0, "scalar %s", type.name);
-        return;
+        return ret;
     }
 
     if(auto enu = cast(const(GQLDEnum))type) {
         enumImpl(o, enu);
-        return;
+        return ret;
     }
+
+    // handle typeImpl for types of members
+    // need to assign to ret so the exit guard updates ah properly
+	if(auto nul = cast(const(GQLDNullable))type) {
+		typeImpl(o, nul.elementType, ah);
+        return ret;
+    } else if(auto op = cast(const(GQLDOperation))type) {
+        typeImpl(o, op.returnType, ah);
+        foreach(val; op.parameters.byValue) {
+            typeImpl(o, val, ah);
+        }
+        return ret = Visibility.outputOnly;
+    } else if(auto l = cast(const(GQLDList))type) {
+        return ret = typeImpl(o, l.elementType, ah);
+    } else if(auto nn = cast(const(GQLDNonNull))type) {
+        return ret = typeImpl(o, nn.elementType, ah);
+    } else if(auto nul = cast(const(GQLDNullable))type) {
+        return ret = typeImpl(o, nul.elementType, ah);
+	}
 
     const map = cast(const(GQLDMap))type;
     if(!map) {
         formIndent(o, 0, "# graphqld couldn't format type '%s' / '%s' / '%s'", type.kind, type.name, type);
-        return;
+        return ret;
     }
     
-    {
-        string typestr = "type";
-        if(auto unio = cast(const(GQLDUnion))type) {
-            typestr = "union";
-        } else if(auto obj = cast(const(GQLDObject))type) {
-            typestr = typeKindToString(obj.typeKind);
-        }
-		formIndent(o, 0, "%s %s {", typestr, type.name);
-	}
-
-    foreach(mem, value; map.member) {
-        if(isNameSpecial(mem)) {
-            continue;
-        }
-        if(auto op = cast(const(GQLDOperation))value) {
-            if (op.parameters.keys().length) {
-                // below causes problems for mutations, so disable temporarily
-                formIndent(o, 1, "# %s(%s): %s", mem,
-                        operationParmsToString(op),
-                        gqldTypeToString(op.returnType));
-            } else {
-                // apparently graphql doesn't allow foo(): bar
-                // so special-case that and turn it into foo: bar
-                formIndent(o, 1, "%s: %s", mem,
-                        gqldTypeToString(op.returnType));
-            }
-        } else {
-            formIndent(o, 1, "%s: %s", mem, gqldTypeToString(value));
+    Visibility[] memberVis;
+    memberVis.reserve(map.member.values.length);
+    foreach(_, val; map.member) {
+        memberVis ~= typeImpl(o, val, ah);
+        if(memberVis[$-1] == Visibility.outputOnly) {
+            ret = Visibility.outputOnly;
         }
     }
 
+    void dumpMem(bool inputType) {
+        size_t memi = 0;
+        foreach(mem, value; map.member) {
+            scope (exit) {
+                memi++;
+            }
+            if(isNameSpecial(mem)) {
+                continue;
+            }
+
+            if(inputType && memberVis[memi] == Visibility.outputOnly) {
+                mem ~= "In";
+            }
+
+            if(auto op = cast(const(GQLDOperation))value) {
+                if (op.parameters.keys().length) {
+                    if(!inputType) {
+                        formIndent(o, 1, "%s(%s): %s", mem,
+                                operationParmsToString(op),
+                                gqldTypeToString(op.returnType));
+                    }
+                } else {
+                    // apparently graphql doesn't allow foo(): bar
+                    // so special-case that and turn it into foo: bar
+                    formIndent(o, 1, "%s: %s", mem,
+                            gqldTypeToString(op.returnType));
+                }
+            } else {
+                formIndent(o, 1, "%s: %s", mem, gqldTypeToString(value));
+            }
+        }
+    }
+
+    string typestr = "type";
+    if(auto unio = cast(const(GQLDUnion))type) {
+        typestr = "union";
+    } else if(auto obj = cast(const(GQLDObject))type) {
+        typestr = typeKindToString(obj.typeKind);
+    }
+
+    formIndent(o, 0, "%s %s {", typestr, type.name);
+    dumpMem(false);
 	formIndent(o, 0, "}");
+
+    if (ret == Visibility.outputOnly) {
+        formIndent(o, 0, "%s %sIn {", typestr, type.name);
+        dumpMem(true);
+        formIndent(o, 0, "}");
+    }
+
+    return ret;
 }
 
 unittest {
